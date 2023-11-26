92112a512aa5e199e2834f34631a5aae859d1631
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2023-11-27 00:19:33 +0100

    lalala

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,10 @@
 import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
-
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { pinecone } from '@/lib/pineconde';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
 const f = createUploadthing();
 
 export const ourFileRouter = {
@@ -20,9 +23,47 @@ export const ourFileRouter = {
                key: file.key,
                name: file.name,
                userId: metadata.userId,
-               url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+               url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+               uploadStatus: 'PROCESSING'
             }
          });
+
+         try {
+            const response = await fetch(
+               `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+            );
+            const blob = await response.blob();
+
+            const loader = new PDFLoader(blob);
+            const pageLevelDocs = await loader.load();
+            const pagesAmt = pageLevelDocs.length;
+
+            const pineconeIndex = pinecone.Index('glyph');
+            const embeddings = new OpenAIEmbeddings({ openAIApiKey: process.env.OPEN_API_KEY });
+
+            await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+               pineconeIndex,
+               namespace: createdFile.id
+            });
+
+            await db.file.update({
+               data: {
+                  uploadStatus: 'SUCCESS'
+               },
+               where: {
+                  id: createdFile.id
+               }
+            });
+         } catch (err) {
+            await db.file.update({
+               data: {
+                  uploadStatus: 'FAILED'
+               },
+               where: {
+                  id: createdFile.id
+               }
+            });
+         }
 
          console.log('Upload complete for userId:', metadata.userId);
 
